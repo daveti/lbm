caa78df119e5dcd2ea01b540cc365de46a2b1314
Dave Tian <dave.jing.tian@gmail.com>
2018-04-17 14:28:50 -0400

    all fields supported for usb

---
+++ frontend/usb.f
@@ -0,0 +1,34 @@
+# Can treat as u32
+usb.pipe
+usb.stream_id
+usb.status
+usb.transfer_flags
+usb.transfer_buffer_length
+usb.actual_length
+usb.setup_packet
+usb.start_frame
+usb.number_of_packets
+usb.interval
+usb.error_count
+usb.devnum
+# Can treat as u32 although the actual length are either u8 or le16
+usb.bcdUSB
+usb.bDeviceClass
+usb.bDeviceSubClass
+usb.bDeviceProtocol
+usb.bMaxPacketSize0
+usb.idVendor
+usb.idProduct
+usb.bcdDevice
+usb.iManufacturer
+usb.iProduct
+usb.iSerialNumber
+usb.bNumConfigurations
+# Strings
+usb.devpath
+usb.manufacturer
+usb.product
+usb.serial
+# Bytes
+usb.data
+usb.request
