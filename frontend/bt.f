93e91ab264ba7a697854aab74b81a230ecc0bffc
Dave Tian <dave.jing.tian@gmail.com>
2018-05-02 14:07:19 -0400

    update bt.f

--- frontend/bt.f
+++ frontend/bt.f
@@ -36,6 +36,34 @@ bt.hci.command.ocf
 bt.hci.command.plen
 	lbm_bluetooth_command_get_plen
 
+# HCI connection (only works for ACL and SCO)
+bt.hci.conn
+	lbm_bluetooth_has_conn	(return 0 or 1; should be called before any other conn helpers for HCI)
+bt.hci.conn.dst
+	lbm_bluetooth_get_conn_dst
+bt.hci.conn.dst_type
+	lbm_bluetooth_get_conn_dst_type
+bt.hci.conn.src
+	lbm_bluetooth_get_conn_src
+bt.hci.conn.src_type
+	lbm_bluetooth_get_conn_src_type
+bt.hci.conn.state
+	lbm_bluetooth_get_conn_state
+bt.hci.conn.mode
+	lbm_bluetooth_get_conn_mode
+bt.hci.conn.type
+	lbm_bluetooth_get_conn_type
+bt.hci.conn.role
+	lbm_bluetooth_get_conn_role
+bt.hci.conn.key_type
+	lbm_bluetooth_get_conn_key_type
+bt.hci.conn.auth_type
+	lbm_bluetooth_get_conn_auth_type
+bt.hci.conn.sec_level
+	lbm_bluetooth_get_conn_sec_level
+bt.hci.conn.io_capability
+	lbm_bluetooth_get_conn_io_capability
+
 # Needs stack allocation and manual assembly
 bt.hci.event.data
 bt.hci.acl.data
